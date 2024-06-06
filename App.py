7b3f8cc738f2ac4b8c88c37bd77ba08a786600f4
RedStoneCraftGG <66346080+RedStoneCraftGG@users.noreply.github.com>
2024-06-07 00:23:18 +0800

    Add args execute

--- App.py
+++ App.py
@@ -1,27 +1,64 @@
 import requests
 import time
 import os
+import argparse
 
 try:
-	chat_id = input("Chat ID Telegram: ")
-	token = input("Token Telegram: ")
-	message = input("Pesan: ")
+	parser = argparse.ArgumentParser(description='Contoh penggunaan aplikasi.')
+	parser.add_argument('-id', '--chatid', help='ID obrolan')
+	parser.add_argument('-t', '--token', help='Token bot')
+	parser.add_argument('-m', '--message', help='Pesan yang akan dikirim')
+	parser.add_argument('-i', '--image', help='URL gambar opsional')
+	parser.add_argument('-d', '--delay', help='Jeda pengiriman dalam detik', type=int)
+	args = parser.parse_args()
 
-	def get_image_url(image_default):
+	if args.chatid:
+		chat_id = args.chatid
+	else:
+		chat_id = input("Chat ID: ")
+
+	if args.token:
+		token = args.token
+	else:
+		token = input("Token Telegram: ")
+
+	if args.message:
+		message = args.message
+	else:
+		message = input("Pesan: ")
+
+	image_url = None
+	image_default = "https://redstone.my.id/bolep.jpeg" # JANGAN DIBUKA!
+
+	if not args.image:
+		if args.chatid and args.token and args.message and not args.delay:
+			image_url = image_default
+		elif args.chatid and args.token and args.message and args.delay:
+			image_url = image_default
+	elif args.image:
+		if args.chatid and args.token and args.message and not args.delay:
+			image_url = args.image
+		elif args.chatid and args.token and args.message and args.delay:
+			image_url = args.image
+	else:
 		image_url = input("Masukan URL gambar (Opsional): ")
 		if not image_url:
 			image_url = image_default
-		return image_url
 
-	cooldown_input = input("Jeda pengiriman dalam detik (default: 10 detik): ")
-	if cooldown_input:
-    		cooldown = float(cooldown_input)
+	if not image_url:
+		image_url = input("Masukan URL gambar (Opsional): ")
+		if not image_url:
+			image_url = image_default
+
+
+
+	if args.delay:
+		cooldown = args.delay
 	else:
-    		cooldown = 10
+		cooldown_input = input("Jeda pengiriman dalam detik (default: 10 detik): ")
+		cooldown = float(cooldown_input) if cooldown_input else 10
 
 	url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
-	image_default = "https://redstone.my.id/bolep.jpeg" # JANGAN DIBUKA!!!
-	image_url = get_image_url(image_default)
 	urlimg = f"https://api.telegram.org/bot{token}/sendPhoto?chat_id={chat_id}&photo={image_url}"
 
 
